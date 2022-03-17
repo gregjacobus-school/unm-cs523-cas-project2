a643823b67f30be99022e3540948e216c5b02a81
gjacobu <gjacobus1@cub.uca.edu>
2022-03-17 17:49:22 -0600

    using bits instead of char strings

--- model.py
+++ model.py
@@ -4,8 +4,6 @@ import random
 from deap import base, creator, tools, algorithms
 from src.amino_acid import AminoAcid
 
-EPITOPE_SIZE = 8
-
 creator.create("FitnessMin", base.Fitness, weights=(-1.0, -1.0)) #TODO weights correct? what are these for?
 creator.create("Epitope", list, fitness=creator.FitnessMin)
 creator.create("Antibody", list, fitness=creator.FitnessMin)
@@ -13,11 +11,26 @@ creator.create("Antibody", list, fitness=creator.FitnessMin)
 def gen_epitope():
     num_amino_acids = random.randint(6,8)
     amino_acids = [AminoAcid().gen_random() for _ in range(num_amino_acids)]
-    return amino_acids
+    epitope_bits = []
+    for amino_acid in amino_acids:
+        epitope_bits.extend(amino_acid.bits)
+    return epitope_bits
 
 toolbox = base.Toolbox()
 toolbox.register("attr_epitope", gen_epitope)
 toolbox.register("epitope", tools.initIterate, creator.Epitope, gen_epitope)
-#toolbox.register("antibody", tools.initRepeat, creator.Antibody)
+toolbox.register("epitope_pop", tools.initRepeat, list, toolbox.epitope)
 
-ep = toolbox.epitope()
+ep_pop = toolbox.epitope_pop(n=100)
+
+toolbox.register("antibody", tools.initRepeat, creator.Antibody)
+toolbox.register("antibody_pop", tools.initRepeat, list, toolbox.antibody)
+#ant_pop = toolbox.antibody_pop(n=100)
+
+#Run the algorithm
+t = 0
+while True:
+    #Do things
+    t += 1
+    if t > 10:
+        break
--- src/amino_acid.py
+++ src/amino_acid.py
@@ -1,11 +1,43 @@
 import random
 class AminoAcid():
+    '''
+    There are 20 different amino acids.
+
+    codon = combo of 3 letters from {A,G,C,U}
+    amino acids: 1 - 6 codons
+    '''
     def __init__(self):
         self.codon = None
 
     def gen_random(self):
         self.codon = [self._random_letter() for _ in range(3)]
+        self._convert_to_bits()
         return self
 
     def _random_letter(self):
-        return random.choice(['A', 'C', 'G', 'U'])
+        return random.choice(['A', 'C', 'G', 'T'])
+
+    def _convert_to_bits(self):
+        '''
+        A = 0
+        C = 1
+        G = 2
+        T = 3
+        '''
+        self.bits = []
+        for letter in self.codon:
+            if letter == 'A':
+                self.bits.append(0)
+                self.bits.append(0)
+            elif letter == 'C':
+                self.bits.append(0)
+                self.bits.append(1)
+            elif letter == 'G':
+                self.bits.append(1)
+                self.bits.append(0)
+            elif letter == 'T':
+                self.bits.append(1)
+                self.bits.append(1)
+            else:
+                raise ValueError(f"Invalid letter '{letter}' in codon")
+        assert len(self.bits) == 6
