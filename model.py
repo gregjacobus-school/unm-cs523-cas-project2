337bb7bfd9258d468b9b3dff3410d47b21a831d6
gjacobu <gjacobus1@cub.uca.edu>
2022-03-17 14:51:24 -0600

    updates

--- model.py
+++ model.py
@@ -6,13 +6,13 @@ from src.amino_acid import AminoAcid
 
 EPITOPE_SIZE = 8
 
-creator.create("FitnessMin", base.Fitness, weights=(-1.0, -1.0)) #TODO weights correct?
+creator.create("FitnessMin", base.Fitness, weights=(-1.0, -1.0)) #TODO weights correct? what are these for?
 creator.create("Epitope", list, fitness=creator.FitnessMin)
 creator.create("Antibody", list, fitness=creator.FitnessMin)
 
 def gen_epitope():
     num_amino_acids = random.randint(6,8)
-    amino_acids = [AminoAcid for _ in range(num_amino_acids)]
+    amino_acids = [AminoAcid().gen_random() for _ in range(num_amino_acids)]
     return amino_acids
 
 toolbox = base.Toolbox()
@@ -20,3 +20,4 @@ toolbox.register("attr_epitope", gen_epitope)
 toolbox.register("epitope", tools.initIterate, creator.Epitope, gen_epitope)
 #toolbox.register("antibody", tools.initRepeat, creator.Antibody)
 
+ep = toolbox.epitope()
--- src/amino_acid.py
+++ src/amino_acid.py
@@ -1,4 +1,11 @@
-
+import random
 class AminoAcid():
     def __init__(self):
-        pass
+        self.codon = None
+
+    def gen_random(self):
+        self.codon = [self._random_letter() for _ in range(3)]
+        return self
+
+    def _random_letter(self):
+        return random.choice(['A', 'C', 'G', 'U'])
