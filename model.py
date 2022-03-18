c3f6350bb30bb90bb995a119dc7dcfe81ae14741
gjacobu <gjacobus1@cub.uca.edu>
2022-03-17 20:30:28 -0600

    adding better binding model

--- model.py
+++ model.py
@@ -3,6 +3,7 @@
 import random
 from deap import base, creator, tools, algorithms
 from src.amino_acid import AminoAcid
+from scipy.spatial.distance import hamming
 
 creator.create("FitnessMin", base.Fitness, weights=(-1.0, -1.0)) #TODO weights correct? what are these for?
 creator.create("Epitope", list, fitness=creator.FitnessMin)
@@ -23,14 +24,42 @@ toolbox.register("epitope_pop", tools.initRepeat, list, toolbox.epitope)
 
 ep_pop = toolbox.epitope_pop(n=100)
 
-toolbox.register("antibody", tools.initRepeat, creator.Antibody)
+#For now, antibodies are represented the same way as epitopes
+toolbox.register("antibody", tools.initIterate, creator.Antibody, gen_epitope)
 toolbox.register("antibody_pop", tools.initRepeat, list, toolbox.antibody)
-#ant_pop = toolbox.antibody_pop(n=100)
+ant_pop = toolbox.antibody_pop(n=100)
+
+def mate(ep, ant):
+    '''Determines if the epitope and antibody should bind'''
+    if ep is None:
+        print("EP was NONE")
+    if ant is None:
+        print("ANT was NONE")
+    if len(ep) != len(ant):
+        return False
+    ham_dist = hamming(ep, ant)
+    if ham_dist < 0.5: #TODO is this a good param?
+        return True
+
+def remove_none_from_list(x):
+    pruned = []
+    for val in x:
+        if val is not None:
+            pruned.append(val)
+    return pruned
+
+#Probability of mating (binding) FIXME what's a good value?
+cxpb = 0.1
 
 #Run the algorithm
-t = 0
-while True:
-    #Do things
-    t += 1
-    if t > 10:
-        break
+NUM_GEN=40
+for _ in range(NUM_GEN):
+    #With some probability, have antibodies bind to epitopes
+    for ant_ind, ant in enumerate(ant_pop):
+        if random.random() < cxpb:
+            ep_ind = random.randint(0, len(ep_pop) - 1)
+            did_mate = mate(ep_pop[ep_ind], ant)
+            if did_mate:
+                ep_pop[ep_ind] = None
+                ep_pop.remove(None)
+print(f"# of antibodies={len(ant_pop)}\n# of epitopes={len(ep_pop)}")
